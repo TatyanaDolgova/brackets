d65602483484ea1b19adc4b8069624670f265acc
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-01-30 09:32:27 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
-// module.exports = 
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
   let stack = [];
   let openBrackets = [];
   let bracketsPair = {};
 
-  bracketsConfig.forEach(i => {
-    openBrackets.push(i[0]);  
+  bracketsConfig.forEach((i) => {
+    openBrackets.push(i[0]);
   });
 
-  bracketsConfig.forEach(i => {
+  bracketsConfig.forEach((i) => {
     bracketsPair[i[1]] = [i[0]].toString();
   });
 
-  console.log(openBrackets);
-  console.log(bracketsPair);
-
-  for(let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
 
-
-    if(openBrackets.includes(currentSymbol)) {
+    if (topElement === bracketsPair[currentSymbol] && topElement !== undefined) {
+      stack.pop();
+    } else if(openBrackets.includes(currentSymbol)) {
       stack.push(currentSymbol);
     } else {
-      if(stack.length === 0) {
-        return false;
-      }
-      let topElement = stack[stack.length - 1];
-      if(topElement === bracketsPair[currentSymbol]) {
-        stack.pop();
-      } else {
+      if (stack.length === 0) {
         return false;
       }
     }
-    
   }
-  console.log(stack);
-}
-
-check('||', [['|', '|']]);
-
+  return stack.length === 0;
+};
